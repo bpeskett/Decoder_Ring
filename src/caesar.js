e22eca52bf2b1b42c901c91465621d964faa4cef
bpeskett <47372446+bpeskett@users.noreply.github.com>
2022-09-30 19:19:25 -0400

    Update caesar.js

--- src/caesar.js
+++ src/caesar.js
@@ -6,19 +6,30 @@
 const caesarModule = (function () 
 {
   // you can add any code you want within this function scope
+  function caesar(input, shift, encode = true) 
+    {
+    
+      if (!shift || shift < -25 || shift > 25)// check for abnormal shift case
+        {
+          return false;
+        }  
+      
+      shift *= encode ? 1 : -1; //to decode in opposite direction
 
-  function caesar(input, shift, encode = true) {
-    // your solution code here
-    if(!shift || shift === 0 || shift < -25 || shift > 25)//checks for abnormal shifts
-      {
-       return false;
-      }
-    for(let i = 0; i < input.length; i++)
-      {
-        
-      }
+      return input.toLowerCase().split("").map((character) => shifter(character, shift)).join("");//uses these methods and helper function to return the encoded or decoded message
+   
+    }
 
-  }
+    function shifter(character, shift) 
+      {
+        const key = "abcdefghijklmnopqrstuvwxyz".split(""); //set key as alphabet where each letter is accessible
+
+        if (!character.match(/[a-z]/)) return character; //returns character without changing it if it is not a letter 
+
+        let index = key.indexOf(character); //uses key to get an index
+        let shifted = (((index + shift) % 26) + 26) % 26; //get remainder of the sum of index and shift, add remainder, add 26
+        return key[shifted];
+      }
 
   return {
     caesar,
